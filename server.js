63f9c370141ffed7a1d25e3ef4628ed26eae30f3
Mostafa Azazi <mostafa.3zazi@gmail.com>
2019-10-25 21:27:08 +0200

    Add chat

--- server.js
+++ server.js
@@ -16,10 +16,37 @@ var count = 0;
 wss.on('connection', (ws) => {
 	console.log('Client connected');
 	count ++;
-	ws.send("hello client" + count);
+	//ws.send("hello client" + count);
 
 	ws.on('message', function(msg) {
-		console.log(msg);
+		var json_data = JSON.parse(msg);
+		console.log(json_data);
+		switch (json_data.type){
+			case "connection_type":
+				ws.type = json_data.text;
+				break;
+			case "username":
+				ws.username = json_data.text;
+				break;
+			case "chat":
+				var chat_message = {
+					type:"chat",
+					text: ws.username + " : " + json_data.text,
+				};
+				wss.clients.forEach(function(client){
+					if(client.type=="client_connection"){
+						client.send(JSON.stringify(chat_message));
+					}
+				});
+				break;
+		}
+
+		// console.log(data);
+		// console.log(wss);
+		// if (msg.data = "client_connection") {wss.type ="client";}
+		// else if (msg.data ="hw_connection") {wss.type ="hw";}
+		
+		
 	});
 	
 	ws.on('close', () => console.log('Client disconnected'));
